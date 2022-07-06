297c929fa6666a9371d77f2149e6b98c6e82be9e
mitydev <michael.tiago@hotmail.com>
2022-07-05 22:10:53 -0300

    Definindo os layouts do app

--- app/src/main/java/com/dio/simulador/DetailActivity2.kt
+++ app/src/main/java/com/dio/simulador/DetailActivity2.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import com.dio.simulador.databinding.ActivityDetail2Binding
 import com.dio.simulador.databinding.ActivityMainBinding
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity2 : AppCompatActivity() {
 
     private lateinit var binding:ActivityDetail2Binding;
 
@@ -13,6 +13,9 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityDetail2Binding.inflate(layoutInflater);
-        setContentView(binding.root);
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar);
+        supportActionBar?.setDisplayHomeAsUpEnabled(true);
     }
 }
